0bee2de5f9c1cd1fc5d2142a25dc3bf8d0a28e3a
Aslonov Davronbek <davronaslonov01@gmail.com>
2025-05-05 01:10:39 +0500

    bugs fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation"; // `usePathname` hook
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./component/Header";
@@ -17,15 +20,50 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState(false);
+  const pathname = usePathname(); // Use `usePathname` to track the route changes
+
+  // Sahifa o'zgarishini kuzatib borish
+  useEffect(() => {
+    setLoading(true); // Sahifa yuklana boshlaydi
+    const timer = setTimeout(() => setLoading(false), 1000); // 1 soniya kutish va loadingni o'chirish
+    return () => clearTimeout(timer); // Cleanup function
+  }, [pathname]); // `pathname` o'zgarishi bilan yuklashni boshla
+
   return (
-    <html lang="en" className="">
-      <body className={` antialiased ${montserrat.className}`}>
+    <html lang="en">
+      <head>
+        <meta name="description" content="Picnic site description here" />
+        <meta name="keywords" content="picnic, outdoors, events, activities" />
+        <meta name="author" content="Your Name" />
+        <meta property="og:title" content="Picnic Site" />
+        <meta
+          property="og:description"
+          content="Join us for an unforgettable picnic experience!"
+        />
+        <meta property="og:image" content="URL to image" />
+        <meta property="og:url" content="https://www.yoursite.com" />
+        <meta name="twitter:title" content="Picnic Site" />
+        <meta
+          name="twitter:description"
+          content="Join us for an unforgettable picnic experience!"
+        />
+        <meta name="twitter:image" content="URL to image" />
+        <title>Picnic Site</title>
+      </head>
+      <body className={`antialiased ${montserrat.className}`}>
+        {loading && (
+          <div className="loading-screen">
+            <div className="loader"></div>
+          </div>
+        )}
         <div className="max-w-full w-full">
           <Header />
 
-          <main className="">
+          <main>
             {children} <ToastContainer />
           </main>
+
           <Footer />
         </div>
       </body>
